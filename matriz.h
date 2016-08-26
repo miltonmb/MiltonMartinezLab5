9913e6b0e174c6a9a9ea79b9e143f64a47aed688
miltonmb <milton.pasos@gmail.com>
2016-08-26 14:34:26 -0600

    Se agrego el constructor

--- matriz.h
+++ matriz.h
@@ -1,7 +1,26 @@
+#pragma once
 #include <iostream>
 #include <stdio>
 #include <string.h>
+
 using namespace std;
+
 class matriz{
-	
+	private:
+		int columna,fila;
+		double **matriz;
+
+	public:
+    	matriz();
+    	matriz(int, int);
+    	matriz(const matriz&);
+    	~matriz();
+
+    	const matriz operator+ (const matriz&)const;
+    	const matriz operator- (const matriz&)const;
+    	const matriz operator* (const matriz&)const;
+
+  	  	void BorrarMatriz();
+    	void llenadoUsuario();
+    	void Transpuesta();	
 };
